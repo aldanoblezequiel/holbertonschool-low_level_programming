ec47c8295048a4de3d33fd34be9ba983c463dfef
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-09-30 12:50:55 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main -First function
+*Return: 0
+*/
+int main(void)
+{
+	printf("size of char%i bytes", sizeof(char));
+	printf("size of int%d bytes", sizeof(int));
+	printf("size of long int%li bytes", sizeof(long int);
+	printf("size of long long int% bytes", sizeof(long long int);
+	printf("size of float%f bytes", sizeof(float);
+	return (0);
+}
