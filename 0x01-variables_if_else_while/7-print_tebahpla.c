b04e04897b13ab5501623354f7fa3054a9739da4
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-01 19:24:14 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - print low reverse
+*Return: 0
+*/
+
+int main(void)
+
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	putchar (n);
+	putchar ('\n');
+	return (0);
+}
