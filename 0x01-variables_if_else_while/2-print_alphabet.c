f6eb647f8c7bb04da4a84193f9c5372f63efe518
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-01 17:59:30 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - print alpha
+*Return: 0
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha  <= 'z' ; alpha++)
+	putchar (alpha);
+	putchar ('\n');
+	return (0);
+}
