9fa97a949e5ecebfb4aace021f1255595831efe1
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-06 16:50:56 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_abs - a function gives last digit form SRAND
+*@j: the int value
+*Return: 1 if c is lowercase and 0 otherwise
+*/
+
+int _abs(int j)
+{
+	if (j >= 0)
+	{
+		return (j);
+	}
+	else
+	{
+		return (j * (-1));
+	}
+}
