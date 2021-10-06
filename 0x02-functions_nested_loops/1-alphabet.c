2bee6b8a12be515298ec0b85d55ec3d485a0dff0
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-06 06:47:54 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_alphabet -Task 1 Function (Print abc loop)
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+	_putchar (alpha);
+	_putchar ('\n');
+}
