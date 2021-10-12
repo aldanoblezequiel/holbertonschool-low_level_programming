f9c68042bd8b4cda74afbb760ca1432767a25e98
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-12 11:00:05 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*_strlen -First function (SIEMPRE LA DESCRIPCION DEL MAIN VA PRIMERO!!!!)
+*Return: 0
+*@s: char variable
+*/
+int _strlen(char *s)
+{
+	_strlen(s);
+	return (0);
+}
