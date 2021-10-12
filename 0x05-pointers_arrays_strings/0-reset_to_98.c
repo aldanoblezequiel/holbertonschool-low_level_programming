371426e01ea0f281fdc0e986d8ec0a83a38542bb
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-12 09:06:27 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
-#include <main.h>
+#include "main.h"
 /**
-*main -First function (SIEMPRE LA DESCRIPCION DEL MAIN VA PRIMERO!!!!)
+*reset_to_98 -First function (SIEMPRE LA DESCRIPCION DEL MAIN VA PRIMERO!!!!)
 *Return: 0
+*@n: entering int
 */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 
 {
-	_putchar(&n = 98);
+	(*n = 98);
 }
