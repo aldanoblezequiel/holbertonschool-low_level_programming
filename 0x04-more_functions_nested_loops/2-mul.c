d3dac20d6e1d594c2907cfa432e9c2c8f1def3a1
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-11 18:45:52 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - this function returns the multiplication
+ * of two integers
+ * @a: entering int
+ * @b: entering int
+ *
+ * Return: int a*b
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
