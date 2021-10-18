dd09efad3a7a5d75fc80c8b34336372b81ebdea7
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-18 12:25:58 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_strchr -First function (SIEMPRE LA DESCRIPCION DEL MAIN VA PRIMERO!!!!)
+*Return: 0
+*@s: string variable
+*@c: char variable
+*/
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != c && s[a] != '\0'; a++)
+	{
+	}
+	return (s);
+}
