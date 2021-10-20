a9d766a9cb3f952fc918ecb193e0f00fc322e71b
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-20 10:11:25 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_strspn -First function (SIEMPRE LA DESCRIPCION DEL MAIN VA PRIMERO!!!!)
+*Return: 0
+*/
+unsigned int _strspn(char *s, char *accept);
+{
+        ;
+        return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+In this project we are getting introduce into the concept of Recursion
