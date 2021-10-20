d260cd11e3845b373819a9b14185906b8ed5c6fe
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-20 11:54:02 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,15 +6,16 @@
 */
 int _strlen_recursion(char *s)
 {
-	int n;
+	int n = 0;
 
 	if (*s < 1)
 	{
 		return ('\0');
 	}
-	for (n = 0; s[n] != '\0'; n++)
+	else
 	{
-		n = _strlen_recursion(s + 1);
+		n++;
+		n += _strlen_recursion(s + 1);
 	}
 	return (n);
 }
