98a351ac7b946702fd661d0a5439b0e2351064d6
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-25 13:57:17 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main -Function that returns its own name
+*@argc: int variable
+*@argv: char variable
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	(void)argv;
+	return (0);
+}
