81579f9bda908ee0e9b6abe36d9c88731cd913f0
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-10-25 18:12:17 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*main -Function that returns its own name
+*@argc: int variable
+*@argv: char variable
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int p;
+
+	if (argc > 0)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		p = i + j;
+		printf("%d\n", p);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
