1f83d29acf99be13c2747f3aaf6c98ad34d9a606
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-03 11:13:22 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size != 0 && array != NULL)
+	if (action || array)
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
