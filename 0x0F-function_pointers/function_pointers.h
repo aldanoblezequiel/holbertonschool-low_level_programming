f071d4d623e97da48b1ce7424e4a5d575c8f7d9f
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-03 10:53:55 -0700

    Task 1

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 #ifndef HEADER
 #define HEADER
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
