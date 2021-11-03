960adfd4f120fd7691731234d208a95052f042c1
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-03 09:29:06 -0700

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*print_name - Function that prints a name
+*@name: Name to print
+*@f: pointer
+*/
+
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This projects is about pointers to functions
