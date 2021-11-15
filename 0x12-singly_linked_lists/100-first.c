2489bf1611e747ea7cf41fe210b31a6768b117f4
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-15 15:19:39 -0800

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * printsxd - prints phrase in main
+  * Return: void
+  */
+void printsxd(void) __attribute__ ((constructor));
+void printsxd(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
