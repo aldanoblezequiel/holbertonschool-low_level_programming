db27ae46407716f1c26f92f9852395ceeffee893
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-16 18:18:51 -0800

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - prints the list
+ * @h: struct
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int counter = 0;
+
+	while (h)
+	{
+		counter++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (counter);
+}
