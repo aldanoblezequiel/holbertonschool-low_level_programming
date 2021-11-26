83a3765165793a64a945950c68e43fb449245403
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2021-11-26 09:11:02 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,26 @@
 #include "main.h"
+
 /**
-*_strspn -Returns the number of bytes in the initial segment of a string
-*Return: 0
+*_strspn - gets the length of a substring
+*@s: number of bytes is extracted
+*@accept: bytes
+*Return: counter
 */
-unsigned int _strspn(char *s, char *accept);
+
+unsigned int _strspn(char *s, char *accept)
 {
-	int count = 0;
+	int c, d;
+	int counter = 0;
 
-	for (int i = 0; s[i] != '\0'; i++)
+	for (c = 0; s[c] != '\0'; c++)
 	{
-		for(int j = 0; j accept[j] != '\0'; j ++)
-
-	return (0);
+		for (d = 0; accept[d] != '\0'; d++)
+		{
+		if (s[c] == accept[d])
+			counter++;
+		}
+		if (s[c] == ' ')
+			break;
+	}
+	return (counter);
 }
