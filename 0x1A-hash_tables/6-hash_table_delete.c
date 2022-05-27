7b595bdf85202eb652320eafc617918b47b52486
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-05-27 10:09:20 -0700

    Task 6 version 4

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,27 +9,23 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int n;
-	hash_node_t *prev, *tmp;
+	hash_node_t *tmp1;
+	hash_node_t *tmp2;
 
-	if (ht)
+	if (ht == NULL)
+		return;
+
+	for (n = 0; n < ht->size; n++)
 	{
-		for (n = 0; n < ht->size; n++)
+		tmp1 = ht->array[n];
+		while ((tmp2 = tmp1) != NULL)
 		{
-			prev = ht->array[n];
-
-			while (prev)
-			{
-				tmp = prev->next;
-
-				free(prev->key);
-				free(prev->value);
-				free(prev);
-
-				prev = tmp;
-			}
+			tmp1 = tmp1->next;
+			free(tmp2->key);
+			free(tmp2->value);
+			free(tmp2);
 		}
-		free(ht->array);
-
-		free(ht);
 	}
+	free(ht->array);
+	free(ht);
 }
