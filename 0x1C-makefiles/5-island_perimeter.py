8100bc9910e1e813ae03ce4005b9a09b4b6e1e31
aldanoblezequiel <aldabaldenoblezequiel@gmail.com>
2022-07-14 19:59:14 -0700

    Task 5 again

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
-"""island_perimeter function"""
+"""
+5-island_perimeter
+"""
+
 
 def island_perimeter(grid):
-    """ 0 represents a water zone
-        1 represent a land zone
     """
+    parameter
+    """
+    perimetro = 0
+
+    # loop de grid
+    for row in range(len(grid)):
+        # loop dentro de linea
+        for col in range(len(grid[0])):
+            # encuentro un 1
+            if grid[row][col] == 1:
+                # manejando primera linea y vecino de arriba
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimetro += 1
+                # manejando ultima linea y vecino de abajo
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
+                    perimetro += 1
+                # manejando primer numero y anterior
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimetro += 1
+                # manejando ultimo numero y siguiente
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
+                    perimetro += 1
+    return perimetro
